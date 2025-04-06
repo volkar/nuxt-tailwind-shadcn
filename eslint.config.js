dc05910325ccc07ba2d5bbfcd73a7030a4660cfc
Sergey Volkar <sergey@volkar.ru>
2025-04-06 15:04:37 +0300

    ESLint

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default defineConfig([
+    { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+    {
+        files: ['**/*.{js,mjs,cjs,ts,vue}'],
+        languageOptions: { globals: { ...globals.browser, ...globals.node } }
+    },
+    {
+        files: ['**/*.{js,mjs,cjs,ts,vue}'],
+        plugins: { js },
+        extends: ['js/recommended']
+    },
+    tseslint.configs.recommended,
+    pluginVue.configs['flat/essential'],
+    {
+        files: ['**/*.vue'],
+        languageOptions: { parserOptions: { parser: tseslint.parser } }
+    },
+    {
+        ignores: ['node_modules', 'dist', '.nuxt', '.output']
+    },
+    eslintConfigPrettier
+])
